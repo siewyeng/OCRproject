7eb2316009f20c26d0080919fcda6b92824c5a0c
Chow Siew Yeng <siew_yeng_chow@aiap.sg>
2024-08-01 17:40:36 +0800

    neaten code

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import torch
 from PIL import Image
 from src.clip.caption_generation import generate2, generate_beam
 from src.clip.model import ClipCaptionModel
-from src.utils import *
+from src.utils import CPU, IS_GPU, PREFIX_LEN, D
 
 # from tqdm import trange
 from transformers import GPT2Tokenizer
@@ -18,6 +18,7 @@ from transformers import GPT2Tokenizer
 
 
 def get_device(device_id: int) -> D:
+    """To retrieve device type and id if GPU"""
     if not torch.cuda.is_available():
         return CPU
     device_id = min(torch.cuda.device_count() - 1, device_id)
@@ -26,26 +27,45 @@ def get_device(device_id: int) -> D:
 
 CUDA = get_device
 
-model_path = "pretrained_models/model_weights.pt"
-device = CUDA(0) if IS_GPU else "cpu"
+MODEL_PATH = "pretrained_models/model_weights.pt"
+DEVICE = CUDA(0) if IS_GPU else "cpu"
 
 
 def load_models():
-    clip_model, preprocess = clip.load("ViT-B/32", device=device, jit=False)
+    """Loads models to be used in main function
+
+    Returns
+    -------
+    loaded pretrained model, gpt2's tokenizer,
+    clip model and the image preprocessing for the clip model
+    """
+    clip_model, preprocess = clip.load("ViT-B/32", device=DEVICE, jit=False)
     tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
     model = ClipCaptionModel(PREFIX_LEN)
 
     model.load_state_dict(
-        torch.load(model_path, map_location=CPU), strict=False
+        torch.load(MODEL_PATH, map_location=CPU), strict=False
     )  # already downloaded weights into folder
 
     model = model.eval()  # do clip caption in eval mode
-    model = model.to(device)
+    model = model.to(DEVICE)
     return model, tokenizer, clip_model, preprocess
 
 
 def process_image(image_path, preprocess, device):
+    """Preprocesses image based on model
+
+    Parameters
+    ----------
+    image_path : str
+    preprocess
+    device : D
+
+    Returns
+    -------
+       preprocessed image
+    """
     pil_image = Image.open(image_path)
     pil_image.show()
     image = preprocess(pil_image).unsqueeze(0).to(device)
@@ -54,12 +74,13 @@ def process_image(image_path, preprocess, device):
 
 # used by server.py
 def infer_caption_from_image(pil_image: Image.Image) -> str:
+    """Infer function used by server"""
     model, tokenizer, clip_model, preprocess = load_models()
     # Preprocess the image
-    image = preprocess(pil_image).unsqueeze(0).to(device)
+    image = preprocess(pil_image).unsqueeze(0).to(DEVICE)
 
     with torch.no_grad():
-        prefix = clip_model.encode_image(image).to(device, dtype=torch.float32)
+        prefix = clip_model.encode_image(image).to(DEVICE, dtype=torch.float32)
         prefix_embed = model.clip_project(prefix).reshape(1, PREFIX_LEN, -1)
 
         # Generate caption (you can use beam search or other methods as needed)
@@ -71,14 +92,21 @@ def infer_caption_from_image(pil_image: Image.Image) -> str:
 
 
 def main(args):
+    """main function to be carried out
+    preprocesses image and generates caption
+
+    Parameters
+    ----------
+    args : arguments specified in command
+    """
     #     image_path = "memes/210513.png"
     # # @title Inference
     use_beam_search = False  # @param {type:"boolean"}
     model, tokenizer, clip_model, preprocess = load_models()
-    image = process_image(args.image_path, preprocess, device)
+    image = process_image(args.image_path, preprocess, DEVICE)
 
     with torch.no_grad():
-        prefix = clip_model.encode_image(image).to(device, dtype=torch.float32)
+        prefix = clip_model.encode_image(image).to(DEVICE, dtype=torch.float32)
         prefix_embed = model.clip_project(prefix).reshape(
             1, PREFIX_LEN, -1
         )  # projects the CLIP embedding into a dimension suitable for GPT2
